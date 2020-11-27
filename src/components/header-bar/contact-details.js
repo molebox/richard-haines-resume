19603dfedb297cb1cb7fe2df6e071c65c144031f
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-11-27 12:12:27 +0100

    updated 27-11-20

--- src/components/header-bar/contact-details.js
+++ src/components/header-bar/contact-details.js
@@ -33,20 +33,6 @@ const ContactDetails = () => {
               color: "headerText"
             }
           }}
-          href="https://garden.richardhaines.dev/"
-        >
-          garden.richardhaines.dev
-        </a>
-      </li>
-      <li>
-        <a
-          sx={{
-            textDecoration: "none",
-            color: "accent",
-            ":hover": {
-              color: "headerText"
-            }
-          }}
           href="https://github.com/molebox"
         >
           github.com/molebox
